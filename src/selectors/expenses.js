c6b19478048f564514356a6afebce81b7bcdd151
Mikey <contact@mikey.codes>
2018-09-24 21:11:25 +0100

    breaking apart the redux store into folders

---
+++ src/selectors/expenses.js
@@ -0,0 +1,19 @@
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof startDate !== 'number' || expense.createdAt <= endDate;;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch & textMatch;
+    }).sort((a, b) => {
+        switch (sortBy) {
+            case 'date':
+                return a.createdAt < b.createdAt ? 1 : -1;
+            case 'amount':
+                return a.amount < b.amount ? 1 : -1;
+        }
+    });
+};
+
+export default getVisibleExpenses;
