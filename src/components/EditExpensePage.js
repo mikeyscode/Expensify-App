b9cf98e8e7932d368b79900454911ab91cdaa408
Mikey <contact@mikey.codes>
2018-10-28 16:24:54 +0000

    fully implemented functionality of redux application for expensify app

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
     return (
         <div>
-            This is from my edit expense component {props.match.params.id}
+            <ExpenseForm 
+                expense={props.expense}
+                onSubmit={(expense) => {
+                    console.log(expense)
+                    props.dispatch(editExpense(props.expense.id, expense));
+                    props.history.push('/');
+                }}
+            />
+            <button onClick={(e) => {
+                e.preventDefault();
+                props.dispatch(removeExpense({ id: props.expense.id }));
+                props.history.push('/');
+            }}>Remove</button>
         </div>
     );
 }
     
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+    }
+};
 
-export default EditExpensePage;
+export default connect(mapStateToProps)(EditExpensePage);
